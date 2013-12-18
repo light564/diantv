e1db4ed697eccf45ff33bbdb18787865721f08eb
light <564774112@qq.com>
2013-12-18 11:16:58 +0800

    20131218

---
+++ blog/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class DianTVBlog(models.Model):
+	Author = models.CharField(max_length = 50)
+	PublishData = models.DateTimeField('date published')
+	Content = models.TextField()
---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+﻿from django.conf.urls import patterns, url
+from blog import views
+
+urlpatterns = patterns('',
+    url(r'^bloglist/$', views.bloglist, name='bloglist'),
+    url(r'^publish/$', views.publish, name='publish'),
+)
