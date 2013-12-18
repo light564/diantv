52d2511b1c946103383967f15dcaca5a7f28485d
light <564774112@qq.com>
2013-12-18 11:08:23 +0800

    20131218
    beta

---
+++ DianTV/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'DianTV.views.home', name='home'),
+    # url(r'^DianTV/', include('DianTV.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^diantv/', include('webpage.urls', namespace="webpage")),
+    url(r'^blog/', include('blog.urls', namespace="blog")),
+)
---
+++ webpage/urls.py
@@ -0,0 +1,7 @@
+﻿from django.conf.urls import patterns, url
+from webpage import views
+
+urlpatterns = patterns('',
+    url(r'^show/', views.show, name='showpage'),
+    url(r'^client/', views.client, name='clientpage'),#in computer
+)
